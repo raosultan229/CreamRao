a4caa8931cee42d76ddb8cbcb4c04dc8ed46a46f
unknown <raosultan229@gmail.com>
2023-01-03 20:08:50 +0500

    first commit

---
+++ frontend/src/components/Announcement.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+// import { mobile } from '../responsive';
+import React from "react";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  height: 30px;
+  // background-image: linear-gradient(#fc575e, #f7b42c);
+  background-color: #eb7100;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 13px;
+  font-weight: 500;
+  ${mobile({ display: "none" })}
+`;
+const Announcement = () => {
+  return <Container>Super Deal! Free Shipping on Orders Over 5000</Container>;
+};
+
+export default Announcement;
